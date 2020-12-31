9d5b0d1a66763a3a786fc549bfecbaf18b78cdae
Mohammad Pirhooshyaran <33163731+mamadpierre@users.noreply.github.com>
2020-12-31 16:51:13 -0600

    Update QuantumNetwork.py

--- QuantumNetwork.py
+++ QuantumNetwork.py
@@ -2,8 +2,10 @@ import pennylane as qml
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from Arguments import get_args
-args = get_args()
+from types import SimpleNamespace
+
+args= SimpleNamespace(device='cpu', n_output=3, n_qubits=4, q_depth=6, small_design = False)
+
 
 dev = qml.device("default.qubit", wires=args.n_qubits)
 @qml.qnode(dev, interface="torch")
@@ -47,6 +49,8 @@ def quantum_net(q_input_features, q_weights_flat, **kwargs):
                 qml.CSWAP(wires=[node, (node + 1) % args.n_qubits, (node + 2) % args.n_qubits])
             if layer_node2 == 'Tof':
                 qml.Toffoli(wires=[node, (node + 1) % args.n_qubits, (node + 2) % args.n_qubits])
+            if layer_node2 == 'CZ':
+                qml.CZ(wires=[node, (node + 1) % args.n_qubits])                
     exp_vals = [qml.expval(qml.PauliZ(position)) for position in range(args.n_output)]
     return tuple(exp_vals)
 
